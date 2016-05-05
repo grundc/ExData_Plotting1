087f4b8db8e8f6193122379bbf548533ec926e54
grundc <christoph.grund@web.de>
2016-05-05 17:20:58 +0200

    Added plot1 as png and R script

---
+++ plot1.R
@@ -0,0 +1,27 @@
+
+# setwd("./git/ExData_Plotting1")
+
+###################################
+# Prepare Data
+###################################
+filepath <- "../data/household_power_consumption.txt"
+
+power <- read.table(filepath,sep=";",header=T, stringsAsFactors = F)
+power$Date <- as.Date(power$Date,"%d/%m/%Y")
+power <- power[power$Date >= as.Date("2007-02-01","%Y-%m-%d") & power$Date <= as.Date("2007-02-02","%Y-%m-%d"), ]
+
+# summary(power)
+
+power$Global_active_power <- as.numeric(power$Global_active_power)
+
+###################################
+# CREATE PLOT 1
+###################################
+
+with(power,hist(Global_active_power, col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power"))
+
+#dev.cur()
+
+png(filename="plot1.png", width=480, height=480)
+with(power,hist(Global_active_power, col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power"))
+dev.off()
